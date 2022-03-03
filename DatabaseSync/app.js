7f75bc4af05b218185ff4aae7984e76964d25e58
Keith Becker <keith.becker@recro.com>
2022-03-02 17:16:41 -0700

    update

--- DatabaseSync/app.js
+++ DatabaseSync/app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
 
-app.get('/listen-for-database-schema', function (req, res) {
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
-
-
+app.post('/listen-for-database-schema', function (req, res) {
+    console.log(req.body);
     res.send('Got database schema')
 })
 
