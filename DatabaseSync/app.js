cd1a8e3eba59269c663bdac5fbe3fdaa68432c2c
Keith Becker <keith.becker@recro.com>
2022-03-02 19:57:19 -0700

    update

--- DatabaseSync/app.js
+++ DatabaseSync/app.js
@@ -36,6 +36,56 @@ async function syncDatabase(data) {
         }
 
 
+        for (let modelIndex = 0; modelIndex < Models.length; modelIndex++) {
+            console.log(Models[modelIndex]);
+
+            let initObject = { firstName: "" };
+            for (let columnIndex = 0; columnIndex < Models[modelIndex].Columns.length; columnIndex++) {
+                initObject[Models[modelIndex].Columns[columnIndex].ColumnName]
+                    = Models[modelIndex].Columns[columnIndex];
+            }
+
+
+
+            let keys = Object.keys(initObject);
+            for (let keysIndex = 0; keysIndex < keys.length; keysIndex++) {
+
+                let subKeys = Object.keys(initObject[keys[keysIndex]])
+
+                for (let subKeysIndex = 0; subKeysIndex < subKeys.length; subKeysIndex++) {
+
+
+                    const toLowerSubProperty = subKeys[subKeysIndex][0].toLowerCase() + subKeys[subKeysIndex].slice(1)
+                    const deleteSubProperty = subKeys[subKeysIndex][0].toUpperCase() + subKeys[subKeysIndex].slice(1)
+
+                    initObject[keys[keysIndex]][(toLowerSubProperty)]
+                        = initObject[keys[keysIndex]][subKeys[subKeysIndex]]
+
+                    delete initObject[keys[keysIndex]][(deleteSubProperty)]
+
+                }
+
+                
+            }
+
+            console.log("initializing table", Models[modelIndex].Name)
+            console.log({ initObject })
+
+
+            const User = sequelize.define(Models[modelIndex].Name, initObject, {
+                sequelize, // We need to pass the connection instance
+                modelName: Models[modelIndex].Name // We need to choose the model name
+                // Other model options go here
+            });
+
+
+
+        }
+
+
+
+
+
     }
 
     
