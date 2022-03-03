74fe0081f5090f164faa06302fa6a503f2484e39
Keith Becker <keith.becker@recro.com>
2022-03-03 06:58:37 -0700

    update

--- DatabaseSync/app.js
+++ DatabaseSync/app.js
@@ -1,19 +1,16 @@
-//const express = require('express')
-//var bodyParser = require('body-parser')
+const express = require('express')
+var bodyParser = require('body-parser')
 
-//const app = express()
+const app = express()
 
-//app.use(bodyParser.json());       // to support JSON-encoded bodies
-//app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//    extended: true
-//}));
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
 
 const { Sequelize, Model, DataTypes } = require('sequelize');
 
 async function syncDatabase(data) {
-    data = {
-        data: '{"Databases":[{"Name":"test","Host":"localhost","Dialect":"mysql","UsernameSecretName":"root","PasswordSecretName":"test","Models":[{"Name":"User","Columns":[{"ColumnName":"firstName","Type":"VARCHAR(100)","AllowNull":false,"DefaultValue":"1","PrimaryKey":true,"Field":"test_field","Unique":"uniqueField","Comment":"This is a Test","Validate":null}]},{"Name":"Account","Columns":[{"ColumnName":"firstName","Type":"VARCHAR(100)","AllowNull":false,"DefaultValue":"1","PrimaryKey":true,"Field":"test_field","Unique":"uniqueField","Comment":"This is a Test","Validate":null},{"ColumnName":"lastName","Type":"VARCHAR(100)","AllowNull":false,"DefaultValue":"1","AutoIncrement":true,"PrimaryKey":true,"Field":"test_field","Unique":"uniqueField","Comment":"This is a Test","Validate":null}]}]}],"Metadata":{"annotations":null,"clusterName":null,"creationTimestamp":"2022-03-02T23:34:01Z","deletionGracePeriodSeconds":null,"deletionTimestamp":null,"finalizers":null,"generateName":null,"generation":1,"labels":null,"managedFields":[{"apiVersion":"medulla.recro.com/v1alpha1","fieldsType":"FieldsV1","fieldsV1":{"f:databases":{}},"manager":"kubectl-create","operation":"Update","subresource":null,"time":"2022-03-02T23:34:01Z"}],"name":"test-1","namespace":"default","ownerReferences":null,"resourceVersion":"472790","selfLink":null,"uid":"777666cd-e799-42be-846e-1ef00c858a7f"},"apiVersion":"medulla.recro.com/v1alpha1","kind":"Data"}'
-    };
     data = JSON.parse(data.data)
     console.log(data);
 
@@ -65,12 +62,10 @@ async function syncDatabase(data) {
 
                 }
 
-                
             }
 
             console.log("initializing table", Models[modelIndex].Name)
             console.log({ initObject })
-
 
             const Model = sequelize.define(Models[modelIndex].Name, initObject, {
                 sequelize, // We need to pass the connection instance
@@ -79,26 +74,15 @@ async function syncDatabase(data) {
             });
 
             await Model.sync({ force: true })
-
         }
-
-
     }
-
-    
-
-
 
 }
 
-(async () => {
-    await syncDatabase()
-})()
 
+app.post('/listen-for-database-schema', async function (req, res) {
+    await syncDatabase(req.body)
+    res.send('Synced database')
+})
 
-//app.post('/listen-for-database-schema', function (req, res) {
-//    console.log(req.body);
-//    res.send('Got database schema')
-//})
-
-//app.listen(3000)
+app.listen(3000)
